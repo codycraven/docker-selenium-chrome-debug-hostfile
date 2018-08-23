c066bd24a1fe37fc079b61556d5bab3e0b8b6a14
Gary Morse <gmorse81@gmail.com>
2018-08-23 08:07:01 -0400

    keeps USER seluser so VNC will work.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 if [ -n $MAP_DOMAINS ]; then
   HOST_IP="$(ping -c 1 host.docker.internal | head -n 1 | cut -d " " -f3 | grep -o -E "[0-9.]+")"
   for i in $(echo $MAP_DOMAINS | sed "s/;/ /g"); do
-    echo "$HOST_IP $i" >> /etc/hosts
+    echo "$HOST_IP $i" | sudo tee -a /etc/hosts > /dev/null
   done
 fi
 
